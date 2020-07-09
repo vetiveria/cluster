d4380d90977be806d8dd97139a2f54b7b6f2169c
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-07-09 23:09:02 +0100

    Update  clustering/projections/interface.py

--- clustering/projections/interface.py
+++ clustering/projections/interface.py
@@ -8,19 +8,29 @@ class Interface:
 
     def __init__(self):
 
-        self.methods = config.methods
+        self.algorithms = config.algorithms
+        self.methods = list(self.algorithms.keys())
 
-    def exc(self, data, method):
+        self.exclude = config.exclude
+        self.identifiers = config.identifiers
+
+    def project(self, data, method):
 
         # Methods
         linear = clustering.projections.linear.Linear()
         kernel = clustering.projections.kernel.Kernel()
 
-        # Reduction
+        # On failure
         failure = 'The string {method} is not a member of {methods}'
+
+        # Reduction
         principals, properties, field = {
-            'PCA': linear.exc(data=data, exclude=['COUNTYGEOID'], identifiers=['COUNTYGEOID']),
-            'KERNEL PCA': kernel.exc(data=data, exclude=['COUNTYGEOID'], identifiers=['COUNTYGEOID'])
+            self.algorithms['LINEAR PCA']: linear.exc(data=data, exclude=self.exclude, identifiers=self.identifiers),
+            self.algorithms['KERNEL PCA']: kernel.exc(data=data, exclude=self.exclude, identifiers=self.identifiers)
         }.get(method, LookupError(failure.format(method=method, methods=self.methods)))
 
         return principals, properties, field
+
+    def exc(self, data, method):
+
+        return self.project(data=data, method=method)
