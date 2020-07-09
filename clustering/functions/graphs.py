13ad6ba49db0f90ccde8f2cd24c0133153883794
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2020-07-09 16:18:00 +0100

    Add clustering/functions/graphs.py: For graphing

---
+++ clustering/functions/graphs.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import seaborn as sns
+
+
+class Graphs:
+    """
+    Class Graphs
+    """
+
+    def __init__(self):
+        """
+        The constructor
+        """
+        self.name = ''
+
+    @staticmethod
+    def scatter(data: pd.DataFrame, x: str, y: str, labels: dict, hue: str = None):
+        """
+
+        :param data: A DataFrame
+        :param x: The abscissae field
+        :param y: The ordinates field
+        :param labels: The dictionary of x & y axis labels
+        :param hue: Differentiates by colour
+
+        :return:
+        """
+        plt.figure(figsize=(4.8, 2.9))
+        plt.tick_params(axis='both', labelsize='large')
+
+        sns.scatterplot(x=x, y=y, data=data, hue=hue)
+
+        plt.xlabel(labels['x'], fontsize='large')
+        plt.ylabel(labels['y'], fontsize='large')
