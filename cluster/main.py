db667c4538601e215be52f92b457b664e306e246
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-21 17:54:11 +0000

    Update cluster/main.py: In progress

--- cluster/main.py
+++ cluster/main.py
@@ -3,8 +3,6 @@ import sys
 
 
 def main():
-    # Ensure the data directories are empty, but exists
-    directories.exc()
 
     # Unload data [parallel]
     unload.exc()
@@ -15,12 +13,6 @@ def main():
     # Scale the data
     scale.exc(data=unscaled)
 
-    # Thinking ... dimension reduction
-    cluster.reduce.Reduce().exc()
-
-    # Thinking ... cluster each projection ... kmc, gmm, bgmm
-    cluster.cluster.Cluster().exc()
-
 
 if __name__ == '__main__':
     root = os.getcwd()
@@ -28,21 +20,12 @@ if __name__ == '__main__':
     sys.path.append(os.path.join(root, 'clustering'))
 
     # Libraries
-    import cluster.matrix.unload
-    import cluster.matrix.read
-    import cluster.matrix.scale
-    import cluster.functions.arguments
-    import cluster.functions.directories
-    import cluster.reductions.interface
-    import cluster.reduce
-    import cluster.cluster
+    import cluster.src.unload
+    import cluster.src.read
+    import cluster.src.scale
 
-    # Instances
-    arguments = cluster.functions.arguments.Arguments()
-    directories = cluster.functions.directories.Directories()
-    unload = cluster.matrix.unload.Unload()
-    read = cluster.matrix.read.Read()
-    scale = cluster.matrix.scale.Scale()
-    interface = cluster.reductions.interface.Interface()
+    unload = cluster.src.unload.Unload()
+    read = cluster.src.read.Read()
+    scale = cluster.src.scale.Scale()
 
     main()
