4906294234d159ab46170ff3b087a57a8654ae35
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-22 18:22:48 +0000

    Add cluster/model/gmm/parameters.py

---
+++ cluster/model/gmm/parameters.py
@@ -0,0 +1,18 @@
+import collections
+
+import numpy as np
+
+
+class Parameters:
+
+    def __init__(self):
+        self.ParametersCollection = collections.namedtuple(typename='ParametersCollection',
+                                                           field_names=['array_n_components', 'array_covariance_type',
+                                                                        'metrics', 'random_state'])
+
+    def exc(self):
+        return self.ParametersCollection(
+            array_n_components=np.arange(3, 9),
+            array_covariance_type=np.array(['full', 'diag', 'tied', 'spherical']),
+            metrics=['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan'],
+            random_state=5)
