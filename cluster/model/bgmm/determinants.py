a6aee87146f9eec3ac8555a49b30b53d28af27e6
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-22 18:19:28 +0000

    Update cluster/model/bgmm/determinants.py

--- cluster/model/bgmm/determinants.py
+++ cluster/model/bgmm/determinants.py
@@ -1,11 +1,6 @@
-import collections
-
-import sklearn.mixture
-
+import dask
 import numpy as np
 import pandas as pd
-
-import dask
 
 import cluster.functions.densities
 import cluster.functions.measures
@@ -26,7 +21,6 @@ class Determinants:
         self.densities = cluster.functions.densities.Densities(matrix=matrix)
         self.measures = cluster.functions.measures.Measures(matrix=matrix)
 
-        
     @dask.delayed
     def properties_(self, model):
         """
@@ -43,10 +37,8 @@ class Determinants:
 
         return pd.DataFrame(data=values, columns=columns)
 
-    
     @dask.delayed
     def densities_(self, model):
-
         return self.densities.exc(model=model)
 
     @dask.delayed
@@ -59,25 +51,19 @@ class Determinants:
         return self.measures.exc(model=model)
 
     @dask.delayed
-    def imprint(self, measures, densities, properties):
-
+    def concatenate(self, measures, densities, properties):
         return pd.concat([measures, densities, properties], axis=1)
 
-    def exc(self):        
-
+    def exc(self):
         calculations = []
 
         for model in self.models:
-
             measures = self.measures_(model=model)
             densities = self.densities_(model=model)
             properties = self.properties_(model=model)
 
-            calculations.append(self.imprint(measures, densities, properties))
+            calculations.append(self.concatenate(measures, densities, properties))
 
         dask.visualize(calculations, filename='calculations', format='pdf')
         values = dask.compute(calculations, scheduler='processes')[0]
         return pd.concat(values, ignore_index=True)
-
-
-
