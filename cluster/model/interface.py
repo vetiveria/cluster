af020dc537920059535b1bdcac05b8aa8e38c0f7
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-23 17:07:40 +0000

    Update cluster/model/interface.py

--- cluster/model/interface.py
+++ cluster/model/interface.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 import cluster.model.bgmm.interface
+import cluster.model.kmc.interface
 
 
 class Interface:
@@ -11,6 +12,7 @@ class Interface:
         """
 
         self.bgmm = cluster.model.bgmm.interface.Interface()
+        self.kmc = cluster.model.kmc.interface.Interface()
 
     def exc(self, modelstr: str) -> pd.DataFrame:
         """
@@ -20,7 +22,8 @@ class Interface:
         """
 
         switch = {
-            'bgmm': self.bgmm.exc()
+            'bgmm': self.bgmm.exc(),
+            'kmc': self.kmc.exc()
         }
 
         return switch.get(modelstr, LookupError("The model '{}' has not been implemented"))
