a7b49705722f23653da8b327541919ee4907b555
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-24 21:48:24 +0000

    Update config.py

--- config.py
+++ config.py
@@ -6,16 +6,31 @@ import os
 class Config:
 
     def __init__(self):
+        """
+        The constructor
+        """
+
         self.root = os.getcwd()
         self.keys = ['linear', 'rbf', 'cosine']
 
     def descriptions_(self):
+        """
+
+        :return:
+        """
+
         Descriptions = collections.namedtuple(typename='Descriptions', field_names=self.keys)
         descriptions = Descriptions('Linear PCA', 'Radial Basis Function Kernel PCA', 'Cosine Kernel PCA')._asdict()
 
         return descriptions
 
     def projections_(self) -> (dict, dict):
+        """
+        For the projection matrices section of the prospective YAML
+
+        :return:
+        """
+
         identifiers = {'COUNTYGEOID': str}
 
         url = 'https://raw.githubusercontent.com/vetiveria/segments/master/warehouse/principals/{}/projections.csv'
@@ -26,8 +41,16 @@ class Config:
 
     @staticmethod
     def design_():
+        """
+        For the design matrix section of the prospective YAML
+
+        :return:
+        """
+
+        identifiers = {'COUNTYGEOID': str}
+
         url = 'https://raw.githubusercontent.com/vetiveria/segments/master/warehouse/design/{}.csv'
         URL = collections.namedtuple(typename='URL', field_names=['data', 'attributes'])
         locators = URL(**{'data': url.format('design'), 'attributes': url.format('attributes')})
 
-        return locators
+        return locators, identifiers
