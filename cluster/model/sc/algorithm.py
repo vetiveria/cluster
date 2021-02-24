ff4c625373b0df894323e53d4bf54be9d5d1cae5
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-24 23:39:48 +0000

    Update cluster/model/sc/algorithm.py

--- cluster/model/sc/algorithm.py
+++ cluster/model/sc/algorithm.py
@@ -12,17 +12,39 @@ import sklearn.cluster
 class Algorithm:
 
     def __init__(self, matrix: np.ndarray, parameters: collections.namedtuple):
+        """
+        
+        :param matrix:
+        :param parameters:
+        """
 
         self.matrix = matrix
         self.parameters = parameters
 
     def modelling(self, n_clusters, eigen_solver, n_init, gamma, affinity, n_neighbours):
+        """
 
-        sklearn.cluster.SpectralClustering(n_clusters=n_clusters, eigen_solver=eigen_solver, n_components=None,
-                                           random_state=self.parameters.random_state, n_init=n_init,
-                                           gamma=gamma, affinity=affinity, n_neighbors=n_neighbours,
-                                           eigen_tol=0.0, assign_labels='discretize', degree=3, coef0=1,
-                                           kernel_params=None, n_jobs=None)
+        :param n_clusters:
+        :param eigen_solver:
+        :param n_init:
+        :param gamma:
+        :param affinity:
+        :param n_neighbours:
+        :return:
+        """
+
+        try:
+            model = sklearn.cluster.SpectralClustering(n_clusters=n_clusters, eigen_solver=eigen_solver,
+                                                       n_components=None,
+                                                       random_state=self.parameters.random_state, n_init=n_init,
+                                                       gamma=gamma, affinity=affinity, n_neighbors=n_neighbours,
+                                                       eigen_tol=0.0, assign_labels='discretize', degree=3, coef0=1,
+                                                       kernel_params=None, n_jobs=None).fit(X=self.matrix)
+        except OSError as _:
+            print('Impossible ... requested clusters: {}, eigenvalue solver: {}'.format(n_clusters, eigen_solver))
+            model = None
+
+        return model
 
     def exc(self):
         """
