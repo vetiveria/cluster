f61b85a5b480aead4da0f2b84cf36b32a0f76201
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-27 22:43:31 +0000

    Update cluster/model/bgmm/parameters.py

--- cluster/model/bgmm/parameters.py
+++ cluster/model/bgmm/parameters.py
@@ -9,12 +9,13 @@ class Parameters:
         self.ParametersCollection = collections.namedtuple(
             typename='ParametersCollection',
             field_names=['array_n_components', 'array_covariance_type', 'array_weight_concentration_prior_type',
-                         'metrics', 'random_state'])
+                         'array_n_init', 'metrics', 'random_state'])
 
     def exc(self):
         return self.ParametersCollection(
             array_n_components=np.array([3, 5, 6]),
             array_covariance_type=np.array(['full', 'diag', 'tied', 'spherical']),
             array_weight_concentration_prior_type=np.array(['dirichlet_process', 'dirichlet_distribution']),
+            array_n_init=np.array([18]),
             metrics=['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan'],
             random_state=5)
