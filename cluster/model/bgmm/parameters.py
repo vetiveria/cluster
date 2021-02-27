3850c4e635b5bb43d5a97bc21b0c05f4f99052bb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-27 12:14:01 +0000

    Update cluster/model/bgmm/parameters.py

--- cluster/model/bgmm/parameters.py
+++ cluster/model/bgmm/parameters.py
@@ -13,7 +13,7 @@ class Parameters:
 
     def exc(self):
         return self.ParametersCollection(
-            array_n_components=np.arange(3, 9),
+            array_n_components=np.array([3, 5, 6]),
             array_covariance_type=np.array(['full', 'diag', 'tied', 'spherical']),
             array_weight_concentration_prior_type=np.array(['dirichlet_process', 'dirichlet_distribution']),
             metrics=['cityblock', 'cosine', 'euclidean', 'l1', 'l2', 'manhattan'],
