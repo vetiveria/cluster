1542bbe23abd2771f41fb75a3ebb4f9e2bbeed62
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-27 22:45:13 +0000

    Update cluster/model/sc/interface.py

--- cluster/model/sc/interface.py
+++ cluster/model/sc/interface.py
@@ -2,7 +2,6 @@ import logging
 import pandas as pd
 import config
 
-import cluster.src.design
 import cluster.src.projections
 
 import cluster.functions.discriminator
@@ -15,9 +14,6 @@ import cluster.model.sc.parameters
 class Interface:
 
     def __init__(self):
-        """
-
-        """
 
         # Configurations
         configurations = config.Config()
@@ -37,30 +33,24 @@ class Interface:
         self.logger = logging.getLogger(__name__)
 
     def exc(self):
-        """
 
-        :return:
-        """
-
-        # The modelling parameters
         parameters = cluster.model.sc.parameters.Parameters().exc()
 
         excerpts = []
+        properties = []
         for key in self.keys:
-
-            if key != 'cosine':
-                continue
 
             # In focus
             self.logger.info('Spectral Clustering: Modelling the {} projections\n'.format(self.descriptions[key]))
 
             # Projection
             projection = self.projections.exc(key=key)
-            self.logger.info('\nTensor Shape: {}\n'.format(projection.tensor.shape))
 
             # The determined models ...
-            models: list = cluster.model.sc.algorithm.Algorithm(matrix=projection.tensor, parameters=parameters).exc()
-            determinants = cluster.model.sc.determinants.Determinants(matrix=projection.tensor, models=models).exc()
+            models: list = cluster.model.sc.algorithm.Algorithm(
+                matrix=projection.tensor, parameters=parameters).exc()
+            determinants = cluster.model.sc.determinants.Determinants(
+                matrix=projection.tensor, models=models).exc()
 
             # The best
             best = self.discriminator.exc(determinants=determinants)
@@ -71,8 +61,14 @@ class Interface:
 
             # Append
             excerpts.append(vector)
+            properties.append(best.properties)
 
         # Concatenate
-        summary = pd.concat(excerpts, axis=0, ignore_index=True)
+        excerpt = pd.concat(excerpts, axis=0, ignore_index=True)
 
-        return summary
+        # Common steps
+        index = excerpt['score'].idxmax()
+        summary = excerpt.iloc[index:(index + 1), :]
+        summary.reset_index(drop=True, inplace=True)
+
+        return summary, properties[index]
