7a5da3ce691a922113e5c506d552800e810ef6ed
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-02-28 01:38:37 +0000

    Add cluster/finale/

---
+++ cluster/finale/prospects.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+import cluster.src.design
+import cluster.src.projections
+
+
+class Prospects:
+
+    def __init__(self, details: pd.Series, supplements: pd.DataFrame):
+        """
+        view = ['r_clusters', 'n_clusters', 'scaled_calinski', 'scaled_davies_transform',
+            'scaled_density', 'score', 'key_description']
+
+        :param supplements:
+        """
+
+        self.details = details
+        self.supplements = supplements
+
+    def labels_(self, matrix):
+
+        # labels
+        if self.details.method == 'sc':
+            labels: np.ndarray = self.details.model.labels_
+        else:
+            labels: np.ndarray = self.details.model.predict(matrix)
+
+        return labels
+
+    def exc(self):
+
+        projections = cluster.src.projections.Projections()
+        projection = projections.exc(key=self.details.key)
+
+        print(self.labels_(matrix=projection.tensor))
+        print(projection.frame.info())
