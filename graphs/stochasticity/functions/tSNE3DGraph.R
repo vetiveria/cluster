e77516361dd65f5aeefc7997c4b04058a0913436
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-04-05 13:42:05 +0100

    Update graphs/stochasticity/functions/tSNE3DGraph.R

--- graphs/stochasticity/functions/tSNE3DGraph.R
+++ graphs/stochasticity/functions/tSNE3DGraph.R
@@ -1,7 +1,8 @@
 
-tSNE3DGraph <- function(data, fileName){
+tSNE3DGraph <- function(data, fileName, colours){
   
-  fig <- plot_ly(data.table(data), x = ~tSNE1, y = ~tSNE2, z = ~tSNE3, color = labels, colors = colours, text = ~paste(county, ', ', state))
+  fig <- plot_ly(data.table(data), x = ~tSNE1, y = ~tSNE2, z = ~tSNE3, color = ~label, 
+                 colors = colours, text = ~paste(county, ', ', state))
   fig <- fig %>% add_markers()
   fig <- fig %>% layout(scene = list(xaxis = list(title = 'tSNE 1'), 
                                      yaxis = list(title = 'tSNE 2'),
