48edeaa421da627baa2eed166230f3b16723f0ab
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-12 19:24:43 +0100

    Update cluster/functions/elbow.py

--- cluster/functions/elbow.py
+++ cluster/functions/elbow.py
@@ -1,21 +1,15 @@
 import cluster.src.projections
 
-import config
-
 import sklearn.cluster
 import yellowbrick.cluster
 
 import logging
 
 
+# noinspection PyUnresolvedReferences
 class Elbow:
 
     def __init__(self):
-
-        # Config
-        configurations = config.Config()
-        self.keys = configurations.keys
-        self.descriptions = configurations.descriptions_()
 
         # Projections
         self.projections = cluster.src.projections.Projections()
@@ -24,24 +18,17 @@ class Elbow:
         logging.basicConfig(level=logging.INFO, format='%(message)s%(asctime)s.%(msecs)03d', datefmt='%Y-%m-%d %H:%M:%S')
         self.logger = logging.getLogger(__name__)
 
-    def exc(self):
+    def exc(self, datum):
 
         model = sklearn.cluster.KMeans()
 
-        for key in self.keys:
+        self.logger.info('Case {}, {}\n'.format(datum.key, datum.description))
 
-            self.logger.info('Case {}, {}\n'.format(key, self.descriptions[key]))
+        projection = self.projections.exc(datum=datum)
 
-            projection = self.projections.exc(key=key)
+        visualizer = yellowbrick.cluster.KElbowVisualizer(
+            model, k=(3, 13), metric='silhouette', timings=True, locate_elbow=True)
+        visualizer.fit(X=projection.tensor)
+        visualizer.show()
 
-            visualizer = yellowbrick.cluster.KElbowVisualizer(
-                model, k=(3, 13), metric='silhouette', timings=True, locate_elbow=True)
-            visualizer.fit(X=projection.tensor)
-            visualizer.show()
-
-            print(visualizer.elbow_value_)
-
-
-
-
-
+        print(visualizer.elbow_value_)
