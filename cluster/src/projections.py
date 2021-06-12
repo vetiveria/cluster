1a31ae8ac933e0b5ce52ca82f86a4e4d73b88633
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-06-12 14:34:37 +0100

    Update cluster/src/projections.py

--- cluster/src/projections.py
+++ cluster/src/projections.py
@@ -3,52 +3,46 @@ import numpy as np
 
 import collections
 
-import config
 
-
-# noinspection PyUnresolvedReferences,PyProtectedMember
 class Projections:
 
     def __init__(self):
-
-        # Configurations
-        configurations = config.Config()
-        self.locators, self.identifiers = configurations.projections_()
-        self.descriptions = configurations.descriptions_()
+        """
+        Constructor
+        """
 
         # Data Class
         self.Projection = collections.namedtuple(typename='Projection',
-                                                 field_names=['frame', 'tensor', 'key', 'description'])
+                                                 field_names=['frame', 'tensor'])
 
-    def frame_(self, key: str) -> pd.DataFrame:
+    @staticmethod
+    def frame_(datum: collections.namedtuple) -> pd.DataFrame:
         """
 
+        :param datum: Note that datum.identifiers is a dict - field name & field data type pairs - that ensures that
+        the identifier fields are read-in correctly
         :return:
         """
 
         try:
-            data = pd.read_csv(filepath_or_buffer=self.locators[key],
-                               header=0, encoding='UTF-8', dtype=self.identifiers)
+            data = pd.read_csv(filepath_or_buffer=datum.url, header=0, encoding='UTF-8', dtype=datum.identifiers)
         except OSError as err:
             raise Exception(err.strerror) from err
 
         return data
 
-    def tensor_(self, frame: pd.DataFrame) -> np.ndarray:
+    def exc(self, datum: collections.namedtuple) -> collections.namedtuple:
         """
+        Notes
+            group -> baseline, cancer, kidney, or ...
+            key -> rbf, cosine, or ...
 
+        :param datum: fields -> [group, key, url, description, identifiers]
         :return:
         """
 
-        return frame.drop(columns=list(self.identifiers.keys())).values
+        # group, key, url, description, identifiers
+        frame: pd.DataFrame = self.frame_(datum=datum)
+        tensor: np.ndarray = frame.drop(columns=datum.identifiers.keys).values
 
-    def exc(self, key: str) -> collections.namedtuple:
-        """
-
-        :return:
-        """
-
-        frame = self.frame_(key=key)
-        tensor = self.tensor_(frame=frame)
-
-        return self.Projection._make((frame, tensor, key, self.descriptions[key]))
+        return self.Projection._make((frame, tensor))
