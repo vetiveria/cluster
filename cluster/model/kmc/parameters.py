294f4e637c4fe71d71fc9182cba9206e7e5fb37f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-10-03 14:23:44 +0100

    Update cluster/model/kmc/parameters.py

--- cluster/model/kmc/parameters.py
+++ cluster/model/kmc/parameters.py
@@ -12,4 +12,4 @@ class Parameters:
 
     def exc(self):
         
-        return self.ParametersCollection(array_n_clusters=np.array([5, 6]), random_state=5)
+        return self.ParametersCollection(array_n_clusters=np.array([5, 6, 7]), random_state=5)
